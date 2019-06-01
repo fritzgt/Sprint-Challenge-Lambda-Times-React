049a7162840e80033c058c88d354eb6f30ee0801
fritzgt <tuanis@live.com>
2019-05-31 21:41:34 -0400

    done  displaying tabs

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import Tab from './Tab';
+import React from "react";
+import Tab from "./Tab";
 const Tabs = props => {
-  return ( <
-    div className = "tabs" >
-    <
-    div className = "topics" >
-    <
-    span className = "title" > TRENDING TOPICS: < /span> {
-      /* map over the tabs provided on your props, create a new Tab component for each one.
-                  give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
-    } {
-      props.tabs.map(item => {
-        <
-        Tab / >
-      })
-    } <
-    /div> <
-    /div>
+  return (
+    <div className="tabs">
+      <div className="topics">
+        <span className="title">TRENDING TOPICS:</span>
+        {/* map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map(item => (
+          <Tab
+            tab={item}
+            selectedTab={props.selectedTab}
+            selectTabHandler={props.selectTabHandler}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
